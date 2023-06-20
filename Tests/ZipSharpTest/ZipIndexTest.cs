2b9dcf3909e1d6b98c865bc991ce38952620fdd5
startewho <hoolooday@live.com>
2023-06-20 13:53:24 +0800

    添加ZipIndex功能

---
+++ Tests/ZipSharpTest/ZipIndexTest.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+using ZipSharp.ZipIndex;
+namespace ZipSharpTest
+{
+    public class ZipIndexTest
+    {
+        [Theory]
+        [InlineData("TestFile\\a.zip","", "border.json", 13630)]
+        [InlineData("TestFile\\a.zip", "", "sub/sub子路径/border.json", 13630)]
+        public void ZipIndexDic(string zipFileName,string password,string fileName,long fileSize)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding encoding = Encoding.UTF8;
+            if (HasChinese(fileName))
+            {
+                encoding = Encoding.GetEncoding("GB2312");
+            }
+
+            var dict = IndexFile.GetIndexs(zipFileName, password,encoding);
+
+            Assert.NotNull(dict);
+
+            var entryPair = dict.First();
+
+            Assert.NotNull(entryPair);
+
+            try
+            {
+                var stream = IndexFile.GetIndexFileStream(zipFileName, password, entryPair.Value, encoding);
+                
+                using (var memory = new MemoryStream())
+                {
+                    stream.CopyTo(memory);
+                    memory.Flush();
+                    stream.Close();
+                    Assert.True(memory.Length == fileSize);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                throw;
+            }
+            
+
+        }
+
+        public static bool HasChinese(string str)
+        {
+            return Regex.IsMatch(str, @"[\u4e00-\u9fa5]");
+        }
+
+    }
+}
---
+++ ZipSharp/ZipIndex/IndexFile.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace ZipSharp.ZipIndex
+{
+    public class IndexFile
+    {
+        /// <summary>
+        /// Name of the file as stored in the zip.
+        /// </summary>
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// Size of compressed data, excluding ZIP headers.
+        /// </summary>
+        public long CompressedSize64 { get; set; }
+
+
+        /// <summary>
+        /// Size of the Uncompressed data.
+        /// </summary>
+        public long UncompressedSize64 { get; set; }
+
+
+        /// <summary>
+        /// Offset where file data header starts.
+        /// </summary>
+        public long Offset { get; set; }
+
+
+        /// <summary>
+        /// CRC of the uncompressed data.
+        /// </summary>
+        public long CRC32 { get; set; }
+
+
+        /// <summary>
+        /// The index of file in the zip file.
+        /// </summary>
+        public long ZipFileIndex { get; set; }
+
+        /// <summary>
+        /// Storage method.
+        /// </summary>
+        public ushort Method { get; set; }  // Storage method.
+
+        /// <summary>
+        /// General purpose bit flag.
+        /// </summary>
+        public ushort Flags { get; set; }
+
+
+        /// <summary>
+        /// Custom data.
+        /// </summary>
+        public Dictionary<string, string> Custom { get; set; }
+
+
+        /// <summary>
+        /// 获取IndexFile字典
+        /// </summary>
+        /// <param name="zipFileName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static Dictionary<string, IndexFile> GetIndexs(string zipFileName, string password, Encoding encoding = null)
+        {
+            var dict = new Dictionary<string, IndexFile>();
+            using Stream inputStream = File.Open(zipFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            using var zipFile = new ZipFile(inputStream, false, StringCodec.Default);
+            if (string.IsNullOrEmpty(password))
+            {
+                zipFile.Password = password;
+            }
+            foreach (var item in zipFile)
+            {
+                var entry = (ZipEntry)item;
+                if (entry.IsFile)
+                {
+                    var indexFile = new IndexFile()
+                    {
+                        Name = entry.Name,
+                        Offset = entry.Offset,
+                        CompressedSize64 = entry.CompressedSize,
+                        UncompressedSize64 = entry.Size,
+                        CRC32 = entry.Crc,
+                        ZipFileIndex = entry.ZipFileIndex,
+                        Method = (ushort)entry.CompressionMethod,
+                        Flags = (ushort)entry.Flags,
+                    };
+                    dict.TryAdd(entry.Name, indexFile);
+                }
+            }
+            return dict;
+        }
+
+        public static Stream GetIndexFileStream(string zipFileName, string password, IndexFile indexFile, Encoding encoding = null)
+        {
+            Stream inputStream = File.Open(zipFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            return GetIndexFileStream(inputStream, password, indexFile, encoding);
+        }
+
+        /// <summary>
+        /// 根据IndexFile获取原文件
+        /// </summary>
+        /// <param name="zipFileStream"></param>
+        /// <param name="password"></param>
+        /// <param name="indexFile"></param>
+        /// <returns></returns>
+        public static Stream GetIndexFileStream(Stream zipFileStream, string password, IndexFile indexFile, Encoding encoding = null)
+        {
+            using var zipFile = new ZipFile(zipFileStream, true, StringCodec.FromEncoding(encoding));
+            if (string.IsNullOrEmpty(password))
+            {
+                zipFile.Password = password;
+            }
+            var entry = new ZipEntry(indexFile.Name)
+            {
+                Offset = indexFile.Offset,
+                CompressedSize = indexFile.CompressedSize64,
+                Size = indexFile.UncompressedSize64,
+                Crc = indexFile.CRC32,
+                ZipFileIndex = indexFile.ZipFileIndex,
+                CompressionMethod = (CompressionMethod)indexFile.Method,
+                Flags = indexFile.Flags,
+            };
+            return zipFile.GetInputStream(entry);
+        }
+
+
+    }
+}
